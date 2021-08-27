093159daf68c7ccdf989cd93a99f53b4e2cf456a
sizhky <1992chinna@gmail.com>
2021-08-27 11:07:47 +0530

    working segmentation

--- auto_train_segmentation/train.py
+++ auto_train_segmentation/train.py
@@ -1,8 +1,9 @@
 from icevision.all import *
-from auto_train_object_detection.custom_functions import *
+from torch_snippets import Glob, parent, P, sys
+sys.path.append(str(P().resolve()))
+from auto_train_segmentation.custom_functions import *
 from torch_snippets.markup import read_json, write_json
 from torch_snippets.registry import Config, registry, AttrDict
-from torch_snippets import Glob, parent
 
 config = Config().from_disk('config.ini')
 config = AttrDict(registry.resolve(config))
@@ -30,6 +31,7 @@ parser = parsers.COCOMaskParser(
 )
 
 data_splitter = RandomSplitter((config.training.train_ratio, 1 - config.training.train_ratio))
+logger.info(f'\nCLASSES INFERRED FROM {config.training.annotations_file}: {parser.class_map}')
 train_records, valid_records = parser.parse(data_splitter)
 
 presize = config.architecture.presize
@@ -41,14 +43,11 @@ valid_tfms = config.testing.preprocess
 train_ds = Dataset(train_records, train_tfms)
 valid_ds = Dataset(valid_records, valid_tfms)
 
-
-# model_type = models.torchvision.retinanet
 extra_args = config.architecture.extra_args
 assert config.architecture.model_type.count('.', 1), "Architecture should look like <base>.<model>"
 a, b = config.architecture.model_type.split('.')
 model_type = getattr(getattr(models, a), b)
 backbone = getattr(model_type.backbones, config.architecture.backbone)(config.architecture.pretrained)
-# backbone = model_type.backbones.resnet50_fpn(pretrained=True)
 model = model_type.model(
     backbone=backbone(pretrained=True), 
     num_classes=len(parser.class_map),
@@ -59,20 +58,48 @@ train_dl = model_type.train_dl(train_ds, batch_size=4, num_workers=4, shuffle=Tr
 valid_dl = model_type.valid_dl(valid_ds, batch_size=4, num_workers=4, shuffle=False)
 # model_type.show_batch(first(valid_dl), ncols=4)
 
-
 metrics = [COCOMetric(metric_type=COCOMetricType.bbox)]
 learn = model_type.fastai.learner(dls=[train_dl, valid_dl], model=model, metrics=metrics)
-# learn.lr_find()
 
-training_scheme = config.training.scheme
+import typer
+app = typer.Typer()
 
-with learn.no_bar():
-    learn.fine_tune(
-        training_scheme.epochs, 
-        training_scheme.lr, 
-        freeze_epochs=training_scheme.freeze_epochs
-    )
+@app.command()
+def find_best_learning_rate():
+    suggested_lrs = learn.lr_find(show_plot=False)
+    recorder = learn.recorder
+    skip_end = 5
+    lrs    = recorder.lrs    if skip_end==0 else recorder.lrs   [:-skip_end]
+    losses = recorder.losses if skip_end==0 else recorder.losses[:-skip_end]
+    fig, ax = plt.subplots(1,1)
+    ax.plot(lrs, losses)
+    ax.set_ylabel("Loss")
+    ax.set_xlabel("Learning Rate")
+    ax.set_xscale('log')
+    fig.savefig(f'{config.project.location}/find_lr_plot.png')
+    logger.info(f'LR Plot is saved at {config.project.location}/find_lr_plot.png')
+    logger.info(f'Suggested LRs: {suggested_lrs.lr_min/10} and {suggested_lrs.lr_steep}')
+    
+@app.command()
+def train_model(lr:float=None):
+    from torch_snippets import load_torch_model_weights_to, save_torch_model_weights_from, makedir
+    try:
+        load_torch_model_weights_to(model, config.training.scheme.resume_training_from)
+    except:
+        logger.info('Training from scratch!')
+        
+    training_scheme = config.training.scheme
 
-from torch_snippets import load_torch_model_weights_to, save_torch_model_weights_from, makedir
-makedir(parent(training_scheme.output_path))
-save_torch_model_weights_from(model, training_scheme.output_path)
+    lr = lr if lr is not None else training_scheme.lr
+    with learn.no_bar():
+        learn.fine_tune(
+            training_scheme.epochs, 
+            lr, 
+            freeze_epochs=training_scheme.freeze_epochs
+        )
+
+    makedir(parent(training_scheme.output_path))
+    save_torch_model_weights_from(model, training_scheme.output_path)
+    
+if __name__ == '__main__':
+    app()
