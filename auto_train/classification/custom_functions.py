6cbe69bedbc1a985799ff9142e503ffee554c2b2
sizhky <1992chinna@gmail.com>
2021-10-06 10:31:40 +0530

    🩹 better labels in imagenette classification

--- auto_train/classification/custom_functions.py
+++ auto_train/classification/custom_functions.py
@@ -36,3 +36,24 @@ class custom_head(nn.Sequential):
     def forward(self, x):
         return self.model(x)
     
+if not hasattr(registry, 'postprocess_function'):
+    registry.create('postprocess_function')
+
+@registry.postprocess_function.register('convert_imagenette_label_mapping')
+def post_process_function():
+    def imagenette_label_mapping(pred):
+        label_mapping = dict(
+            n01440764='tench',
+            n02102040='English springer',
+            n02979186='cassette player',
+            n03000684='chain saw',
+            n03028079='church',
+            n03394916='French horn',
+            n03417042='garbage truck',
+            n03425413='gas pump',
+            n03445777='golf ball',
+            n03888257='parachute'
+        )
+        pred['prediction'] = label_mapping[pred['prediction']]
+        return pred
+    return imagenette_label_mapping
